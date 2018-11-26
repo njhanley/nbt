ee002d78abdfea877dcb1804e7a4d1cfc796afda
njhanley <njhanley@users.noreply.github.com>
2018-11-26 00:16:30 -0500

    add type conversion helpers to NamedTag and List

--- types.go
+++ types.go
@@ -44,9 +44,105 @@ type NamedTag struct {
 	Payload interface{}
 }
 
+func (tag *NamedTag) ToByte() int8 {
+	return tag.Payload.(int8)
+}
+
+func (tag *NamedTag) ToShort() int16 {
+	return tag.Payload.(int16)
+}
+
+func (tag *NamedTag) ToInt() int32 {
+	return tag.Payload.(int32)
+}
+
+func (tag *NamedTag) ToLong() int64 {
+	return tag.Payload.(int64)
+}
+
+func (tag *NamedTag) ToFloat() float32 {
+	return tag.Payload.(float32)
+}
+
+func (tag *NamedTag) ToDouble() float64 {
+	return tag.Payload.(float64)
+}
+
+func (tag *NamedTag) ToByteArray() []byte {
+	return tag.Payload.([]byte)
+}
+
+func (tag *NamedTag) ToString() string {
+	return tag.Payload.(string)
+}
+
+func (tag *NamedTag) ToList() *List {
+	return tag.Payload.(*List)
+}
+
+func (tag *NamedTag) ToCompound() Compound {
+	return tag.Payload.(Compound)
+}
+
+func (tag *NamedTag) ToIntArray() []int32 {
+	return tag.Payload.([]int32)
+}
+
+func (tag *NamedTag) ToLongArray() []int64 {
+	return tag.Payload.([]int64)
+}
+
 type List struct {
 	Type  Type
 	Array interface{}
+}
+
+func (list *List) ToByte() []int8 {
+	return list.Array.([]int8)
+}
+
+func (list *List) ToShort() []int16 {
+	return list.Array.([]int16)
+}
+
+func (list *List) ToInt() []int32 {
+	return list.Array.([]int32)
+}
+
+func (list *List) ToLong() []int64 {
+	return list.Array.([]int64)
+}
+
+func (list *List) ToFloat() []float32 {
+	return list.Array.([]float32)
+}
+
+func (list *List) ToDouble() []float64 {
+	return list.Array.([]float64)
+}
+
+func (list *List) ToByteArray() [][]byte {
+	return list.Array.([][]byte)
+}
+
+func (list *List) ToString() []string {
+	return list.Array.([]string)
+}
+
+func (list *List) ToList() []*List {
+	return list.Array.([]*List)
+}
+
+func (list *List) ToCompound() []Compound {
+	return list.Array.([]Compound)
+}
+
+func (list *List) ToIntArray() [][]int32 {
+	return list.Array.([][]int32)
+}
+
+func (list *List) ToLongArray() [][]int64 {
+	return list.Array.([][]int64)
 }
 
 type Compound map[string]*NamedTag
